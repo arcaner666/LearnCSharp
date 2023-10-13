bb20fa9cc624aa5e424d787938f7a260ec469b1b
arcaner666 <arcaner666@gmail.com>
2023-10-13 17:31:29 +0300

    updated

--- 07-BasicConcepts/01-ApplicationStartup/02-Application/Application/Program.cs
+++ 07-BasicConcepts/01-ApplicationStartup/02-Application/Application/Program.cs
@@ -8,29 +8,86 @@ namespace Application
     {
         #region Tanım
         /*
-        C#'ta bir programın application olarak (compiling) derlenebilmesi için içerisinde bulunan en az bir metodun alttaki şartları karşılaması gerekir:
-        - Metodun adı Main olmalıdır.
-        - static bir metot olmalıdır.
-        - generic bir metot olamaz.
-        - Eğer metotta belirtilen tip (nested) içiçe bir tipse onu kapsayan tiplerin hiçbiri generic olamaz.
-        - Dönüş tipi System.Threading.Tasks.Task ya da System.Threading.Tasks.Task<int> olarak belirtilirse async deyimi bulundurabilir.
-        - Dönüş tipleri void, int, System.Threading.Tasks.Task, ya da System.Threading.Tasks.Task<int> olmalıdır.
+        Bir program, diğer uygulamaların bir parçası olarak kullanılacak bir sınıf kitaplığı (class library) olarak veya doğrudan başlatılabilen bir uygulama (executable/application) 
+        olarak derlenebilir (compiling). Bu derleme modunu belirleme mekanizması uygulamaya özeldir ve bu spesifikasyonun dışındadır.
+        
+        C#'ta bir programın uygulama (executable/application) olarak derlenebilmesi için içerisinde bulunan en az bir metodun alttaki şartları karşılaması gerekir:
+        - Main adını taşımalı
+        - static olmalı
+        - generic olmayan bir türde tanımlanmalı
+        - Eğer tanımladığı tür içiçe tanımlanmış (nested) bir türse onu kapsayan türlerin hiçbiri generic olamaz
+        - Dönüş türünün System.Threading.Tasks.Task veya System.Threading.Tasks.Task int olması durumunda async bildirgecine sahip olabilir.
+        - Dönüş türü void, int, System.Threading.Tasks.Task veya System.Threading.Tasks.Task int olmalı
+        - İmplement edilmemiş bir partial metot olamaz
+        - Parametre olarak ya boş olmalı ya da string[] türünde tek bir parametreye sahip olmalıdır.
+
+        Genelde bir metodun veya türün (type) erişilebilirliği tanımındaki erişim bildirgecine (access modifier) bağlıdır. 
+        Bir tür içerisindeki bir metodun çağrılabilmesi için hem türün hem de medotun erişim bildirgecinin uygun olması gerekir.
+        Ancak uygulamanın başlangıç noktasında (application entry point) böyle bir durum söz konusu değildir. 
+        Spesifik olarak çalışma ortamı (execution environment) metot tanımında belirtilen erişim bildirgecinden veya metodu kapsayan türün erişim bildirgecinden 
+        bağımsız olarak bu metoda erişebilir.
+
+        Eğer başlangıç metodunun dönüş türü System.Threading.Tasks.Task, ya da System.Threading.Tasks.Task<int> ise derleyici bu metodu çağıran yeni senkron
+        bir metot sentezler. Bu kapsayıcı metodun dönüş türü de System.Threading.Tasks.Task için void, System.Threading.Tasks.Task<int> içinse int olur.
+
+        Bu sentezlenen metodun çalışma şekli aşağıdaki gibidir.
+        - Sentezlenen metot Main metodunu çağırır, içinde gelen string[] parametresini eğer Main'de de böyle bir parametre varsa ona parametre olarak girer.
+        - Eğer Main metodu bir exception döndürürse sentezlenen metot da bu exception'ı döndürür.
+        - Diğer durumda sentezlenen metot Main metodundan dönen sonucu bekler, Task üzerindeki GetAwaiter().GetResult metotlarını parametreli veya parametresiz olarak çağırır.
+        Eğer Task hata döndürürse bu hata sentezlenen metot da döndürülür.
+        - System.Threading.Tasks.Task<int> döndüren Main metodu ise başlarılı olduğunda sentezlenen metoda int döndürür.
+
+        Bir uygulamanın efektif başlangıç noktası (the effective entry point) ya kurallara uygun bir Main metodudur ya da üstteki gibi sentezlenmiş bir metottur.
+        Efektif başlangıç noktası her zaman ya void ya da int dönderir.
+
+        Bir uygulama çalışmaya başladığında yeni bir uygulama etki alanı (application domain) oluşturulur. Bir cihazda aynı anda aynı uygulama birden fazla kez çalıştırılabilir ve
+        her çalıştırma için ayrı bir uygulama etki alanı oluşturulur. Uygulama etki alanı uygulamaların farklı oturumlarını birbirinden izole eder ve 
+        uygulamanın durumunun (application state) korunmasını sağlar. Bir uygulama etki alanı, uygulamada tanımlanan türler ve kullandığı sınıf kitaplıkları için bir kapsayıcı 
+        ve sınır görevi görür. Bir uygulama etki alanına yüklenen türler, başka bir uygulama etki alanına yüklenen türlerden farklıdır ve nesnelerin örnekleri, 
+        uygulama etki alanları arasında doğrudan paylaşılmaz. Örneğin, her uygulama etki alanı, bu türler için kendi static değişken kopyalarına sahiptir ve bir tür için static
+        constructor her uygulama etki alanında en fazla bir kez çalıştırılır.
+
+        Uygulama, yürütme ortamı (execution environment) uygulamanın etkin giriş noktasını (effective entry point) çağırdığında başlar. Etkin giriş noktası bir parametreye sahipse, 
+        uygulamanın başlatılması sırasında, bu parametrenin boş olmayan (non-null) bir string dizisi olmadığından emin olur. Bu dizi, uygulama başlatılmadan önce yürütme ortamı
+        tarafından uygulama girilir ve bu diziye uygulama parametreleri (application parameters) adı verilir. Burada amaç, uygulama başlatılmadan önce uygulamaya ortam dışından bir girdi 
+        sağlayabilmektir.
+
+        Etkin giriş noktasının dönüş türü int ise, buradan dönen değer, uygulamanın sonlandırılmasında kullanılır.
+
+        Yukarıda sayılan durumlar dışında giriş noktası her bakımdan normal metotlar gibi davranır. Özellikle, giriş noktası uygulamanın ömrü boyunca 
+        herhangi bir noktada tekrar çağrılırsa herhangi özel bir etkiye sebep olmaz. Örneğin; bir parametresi varsa, başlangıç değeri null olabilir veya null 
+        referanslar içeren bir diziye referans gösterebilir. Benzer şekilde, giriş noktasının dönüş değerinin, yürütme ortamından yapılan çağrı dışında özel bir önemi yoktur.
         */
         #endregion
 
-        #region Doğru Kullanımlar
+        #region Doğru Kullanım 1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
         #endregion
 
-        #region Yanlış Kullanımlar
-        // --- YANLIŞ KULLANIMLAR ---
-        // static yerine public veya başka bir erişim bildigeci verilirse hata alırız.
-        //public void Main(string[] args)
+        #region Doğru Kullanım 2
+        //static int Main(string[] args)
         //{
         //    Console.WriteLine("Hello, World!");
+        //    return 0;
+        //}
+        #endregion
+
+        #region Doğru Kullanım 3
+        //static async Task Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello, World!");
+        //}
+        #endregion
+
+        #region Doğru Kullanım 4
+        //static async Task<int> Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello, World!");
+        //    await Task.Delay(100);
+        //    return 0;
         //}
         #endregion
     }
---
+++ 07-BasicConcepts/02-ApplicationTermination/Application/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Uygulamanın sonlandırılması, kontrolü yürütme ortamına geri döndürür.
+
+Uygulamanın etkin giriş noktası metodunun dönüş türü int ise ve yürütme bir istisnaya (exception) yol açmadan tamamlanırsa, döndürülen int'nin değeri 
+uygulamanın sonlandırma durum kodu (termination status code) olarak görev yapar. Bu kodun amacı, başarı veya başarısızlığın yürütme ortamına iletilmesine olanak sağlamaktır.
+Etkin giriş noktası metodunun dönüş türü void ise ve yürütme bir istisnayla (exception) sonuçlanmadan tamamlanırsa sonlandırma durum kodu (termination status code) 0'dır.
+Etkin giriş noktası metodu bir istisna nedeniyle sona ererse, çıkış kodu uygulamaya özeldir.
+Sonlandırıcıların (finalizers) uygulama sonlandırmasının bir parçası olarak çalıştırılıp çalıştırılmayacağı uygulamaya özeldir.
+*/
+namespace Application
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            return 0;
+        }
+    }
+}
