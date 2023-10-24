80b5614f07ea6cea35e5e8e6bf92297ea7019c9a
arcaner666 <arcaner666@gmail.com>
2023-10-24 22:04:56 +0300

    updated

--- 07-BasicConcepts/04-Members/Application/Program.cs
+++ 07-BasicConcepts/04-Members/Application/Program.cs
@@ -8,8 +8,8 @@ Ad uzayları (namespace) ve türlerin (types) üyeleri (members) vardır. Bir va
 Bir türün üyeleri ya türün içerisinde ya da bu türün temel sınıfında tanımlanır. Bir tür temel sınıfından miras aldığında, örnek oluşturucular (instance constructors),
 sonlandırıcılar (finalizers) ve statik oluşturucular (static constructors) dışında temel sınıfın tüm üyeleri, türetilen türün üyeleri haline gelir.
 
-Bir temel sınıftaki üyenin erişim bildirgeci (access modifier), üyenin miras alınıp alınmadığını denetlemez. Erişim bildirgeçleri örnek oluşturucu, statik oluşturucu
-veya sonlandırıcı hariç geri kalan tüm üyelere yayılır.
+Bir temel sınıftaki üyenin erişim bildirgeci (access modifier), üyenin miras alınıp alınmayacağını kontrol edemez. Erişim bildirgecinden bağımsız olarak temel sınıfın 
+örnek oluşturucu, statik oluşturucu veya sonlandırıcı hariç geri kalan tüm üyeleri türetilen sınıfın üyeleri haline getirir.
 
 Yine de, temel bir sınıf içerisindeki bir üyeye üzerinde tanımlanan erişim bildirgeci sebebiyle türetildiği sınıftan erişilemeyebilir. 
 */
@@ -24,15 +24,14 @@ Yine de, temel bir sınıf içerisindeki bir üyeye üzerinde tanımlanan erişi
 //        NameSurname = nameSurname;
 //        Age = age;
 //    }
-
 //    public void ShowMyNameAndAge()
 //    {
 //        Console.WriteLine($"{NameSurname} is {Age} years old.");
 //    }
 //}
-
-//// Person sınıfından miras alındığından ve NameSurname, Age, ShowMyNameAndAge üyelerinin erişim bildirgeci public olduğundan bunlar Customer sınıfına aktarılmıştır.
-//// BirthDate üyesinin erişim bildirgeci private olduğundan aktarılmamıştır.
+//// Person sınıfından miras alındığından ve NameSurname, Age, ShowMyNameAndAge, BirthDate üyelerinin erişim bildirgeçlerinden bağımsız olarak hepsi Customer sınıfına
+//// aktarılmıştır. Diğer üç üyenin erişim bildirgeci public olduğu için bunlara Customer sınıfından erişilebilirken BirthDate üyesinin erişim bildirgeci private olduğundan
+//// erişilemiyor.
 //public class Customer : Person
 //{
 //    // Bu sadece Customer sınıfında bulunan bir alandır. Sadece Customer sınıfında tanımlandığı için Person sınıfında bulunmaz.
@@ -43,7 +42,6 @@ Yine de, temel bir sınıf içerisindeki bir üyeye üzerinde tanımlanan erişi
 //        OrderCount = orderCount;
 //    }
 //}
-
 //namespace Application
 //{
 //    internal class Program
@@ -55,9 +53,7 @@ Yine de, temel bir sınıf içerisindeki bir üyeye üzerinde tanımlanan erişi
 //            Console.WriteLine($"Name Surname: {person.NameSurname}");
 //            Console.WriteLine($"Age: {person.Age}");
 //            person.ShowMyNameAndAge();
-
 //            Console.WriteLine("------------------------------");
-
 //            var customer = new Customer("Yaşar Taner Şenol", 21, 5);// Örnek oluşturucular miras alınmadığından ikisi sınıf farklı bilgiler içerebilir.
 //            Console.WriteLine($"Name Surname: {customer.NameSurname}");
 //            Console.WriteLine($"Age: {customer.Age}");
@@ -123,7 +119,7 @@ Ad uzaylarının erişim kısıtlaması yoktur. Ad uzaylarında private, protect
 
 #region Yapı Üyeleri (Struct Members)
 /*
-Bir yapının (struct) üyeleri yapı içerisinde tanımlanan üyeler, yapının direk temel sınıfı (direct base class) olan System.ValueType ve dolaylı temel sınıfı olan
+Summary: Bir yapının (struct) üyeleri yapı içerisinde tanımlanan üyeler, yapının direk temel sınıfı (direct base class) olan System.ValueType ve dolaylı temel sınıfı olan
 (indirect base class) object üyelerinden oluşur.
 
 Basit bir türün (simple type) üyeleri, basit tür (§8.3.5) tarafından takma ad (alias) verilen yapı türünün üyelerine doğrudan karşılık gelir.
@@ -140,17 +136,110 @@ Basit bir türün (simple type) üyeleri, basit tür (§8.3.5) tarafından takma
 //            NameSurname = nameSurname;
 //        }
 //    }
-
 //    internal class Program
 //    {
 //        static void Main(string[] args)
 //        {
 //            var person = new Person("Arcan Caner Şenol");
-//            // Burada yapı içerisinde tanımlanan NameSurname alanına erişilmiştir.
-//            Console.WriteLine($"Name Surname: {person.NameSurname}");
+//            // Yapı içerisindeki bizim tanımladığımız üyeler
+//            Console.WriteLine($"Name Surname => {person.NameSurname}");
+
+//            // System.ValueType direk temel sınıfından gelen üyeler
+//            // .Equals() metodu üyesi olduğu örnekle (instance) içerisine parametre olarak girilen nesneyi (object) karşılaştırır. Duruma göre true ya da false döndürür.
+//            Console.WriteLine($".Equals() => {person.Equals(new Person("Arcan Caner Şenol"))}");
+//            // .GetHashCode() metodu üyesi olduğu örneğin şifreli kodunu (hash code) geri döndürür.
+//            Console.WriteLine($".GetHashCode() => {person.GetHashCode()}");
+//            // .ToString() metodu üyesi olduğu örneğin uzun adını dize (string) tipinde geri döndürür.
+//            Console.WriteLine($".ToString() => {person.ToString()}");
+
+//            // object dolaylı temel sınıfından gelen üyeler
+//            // .GetType() metodu üyesi olduğu örneğin türünü (type) döndürür.
+//            Console.WriteLine($".GetType() => {person.GetType()}");
 //        }
 //    }
 //}
+#endregion
+
+#region Sayılabilir Üyeleri (Enumeration Members)
+/*
+Bir sayılabilirin (enumerable) üyeleri sayılabilir içerisinde tanımlanan sabitlerden (constants), sayılabilirin direk temel sınıfı (direct base class) olan System.Enum, dolaylı temel sınıfı olan
+(indirect base class) object ve System.ValueType üyelerinden oluşur.
+*/
+
+//namespace Application
+//{
+//    public enum DaysOfTheWeek
+//    {
+//        Monday = 1,
+//        Tuesday,
+//        Wednesday,
+//        Thursday,
+//        Friday,
+//        Saturday,
+//        Sunday,
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Sayılabilir içerisinde bizim tanımladığımız üyeler
+//            Console.WriteLine($"Day Name: {DaysOfTheWeek.Monday}, Day Number: {(int)DaysOfTheWeek.Monday}");
+//            Console.WriteLine($"Day Name: {DaysOfTheWeek.Tuesday}, Day Number: {(int)DaysOfTheWeek.Tuesday}");
+//            Console.WriteLine($"Day Name: {DaysOfTheWeek.Wednesday}, Day Number: {(int)DaysOfTheWeek.Wednesday}");
+//            Console.WriteLine($"Day Name: {DaysOfTheWeek.Thursday}, Day Number: {(int)DaysOfTheWeek.Thursday}");
+//            Console.WriteLine($"Day Name: {DaysOfTheWeek.Friday}, Day Number: {(int)DaysOfTheWeek.Friday}");
+//            Console.WriteLine($"Day Name: {DaysOfTheWeek.Saturday}, Day Number: {(int)DaysOfTheWeek.Saturday}");
+//            Console.WriteLine($"Day Name: {DaysOfTheWeek.Sunday}, Day Number: {(int)DaysOfTheWeek.Sunday}");
+
+//            // System.Enum direk temel sınıfından gelen üyeler
+//            // .GetValuesAsUnderlyingType() metodu üyesi olduğu sayılabilirin içindeki üyeleri verilen türde Array olarak geri döndürür.
+//            Console.WriteLine($".GetValuesAsUnderlyingType() => {DaysOfTheWeek.GetValuesAsUnderlyingType(typeof(DaysOfTheWeek))}");
+//            // Bu metodun alttaki ve üsteki şekillerde iki farklı imzası (method signature) vardır.
+//            Console.WriteLine($".GetValuesAsUnderlyingType() => {DaysOfTheWeek.GetValuesAsUnderlyingType<DaysOfTheWeek>()}");
+//            // .GetValuesAsUnderlyingType() metodu bizim tanımladığımız sayılabilir yerine onun temel sınıfındaki statik bir metot olduğundan alttaki şekilde de erişilebilir.
+//            Console.WriteLine($".GetValuesAsUnderlyingType() => {Enum.GetValuesAsUnderlyingType<DaysOfTheWeek>()}");
+//            var values = Enum.GetValuesAsUnderlyingType<DaysOfTheWeek>();
+//            Console.WriteLine($"values => {values}");
+//            Console.WriteLine($"values.GetValue(0) => {values.GetValue(0)}");
+//            Console.WriteLine($"values.GetValue(1) => {values.GetValue(1)}");
+//            // !!! .TryParse() metodunun ne işe yaradığını tam olarak anlayamadım. Sonra tekrar dönülecek !!!
+//            Console.WriteLine($".TryParse() => {DaysOfTheWeek.TryParse(typeof(DaysOfTheWeek), "Monday", false, out var result)}");
+//            Console.WriteLine($".TryParse() => {result.GetType() == typeof(DaysOfTheWeek)}");
+//            Console.WriteLine($".TryParse() => {result}");
+//        }
+//    }
+//}
+#endregion
+
+#region Sınıf Üyeleri (Class Members)
+/*
+Bir sınıfın üyeleri, sınıfta tanımlanan üyeler ve temel sınıftan miras alınan üyelerdir -temel sınıfı (base class) olmayan object sınıfı hariç-. Temel sınıftan 
+miras alınan üyeler, temel sınıfın sabitlerini (constants), alanlarını (fields), metotlarını (methods), özelliklerini (properties), olaylarını (events), dizin 
+oluşturucularını (indexers), operatörlerini (operators) ve türlerini (types) içerir, ancak temel sınıfın örnek oluşturucularını (instance constructors), sonlandırıcılarını
+(finalizers) ve statik oluşturucularını (static constructors) içermez. Temel sınıfın tüm üyeleri, türetilen sınıf tarafından erişilebilirliklerine bakılmaksızın miras 
+alınır.
+
+object (§8.2.3) ve string (§8.2.5) üyeleri, takma ad (alias) olarak kullanıldıkları sınıf türlerinin üyelerini doğrudan alırlar.
+
+Önceki kısımlarda bu bölümü kapsayan çokça örnek var buraya özel yeni bir örneğe ihtiyaç yok.
+*/
+#endregion
+
+#region Arayüz Üyeleri (Interface Members)
+/*
+Bir arayüzün (interface) üyeleri, arayüzde ve arayüzün tüm temel arayüzlerinde tanımlanan üyelerdir.
+
+object sınıfındaki üyeler, kesin olarak herhangi bir arayüzün üyesi değildir (§18.4). object sınıfındaki üyelere herhangi bir arayüz türünde üye arama yoluyla 
+ulaşılabilir (§12.5).
+
+Önceki kısımlarda bu bölümü kapsayan çokça örnek var buraya özel yeni bir örneğe ihtiyaç yok.
+*/
+#endregion
+
+#region Dizi Üyeleri (Array Members)
+/*
+Bir dizinin üyeleri System.Array sınıfından miras alınan üyelerdir.
+*/
 #endregion
 
 #endregion
